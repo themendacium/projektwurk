35178be519c6067008452c151d83b4cc7b7d3f75
themendacium <118759372+themendacium@users.noreply.github.com>
2022-11-21 23:20:17 +0530

    Update word.java

--- word.java
+++ word.java
@@ -1,4 +1,31 @@
 import java.util.*;
 class word {
+	String st;
+	word() {
+		st = "";
+	}
+	void input() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a line = ");
+		st = sc.nextLine().trim();
+	}
+	int count_vowel(String w) {
+		int c = 0;
+		w = w.toUpperCase();
+		for(int i = 0; i < w.length(); ++i) {
+			switch(w.charAt(i)) {
+				case 'A':
+				case 'E':
+				case 'I':
+				case 'O':
+				case 'U':
+					++c;
+					break;
+				default:
+					return 0;
+			}
+		}
+		return c;
+	}
 	
 }
